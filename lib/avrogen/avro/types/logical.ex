b485d0af138781b0480a14b83c00938443a08441
Ed Fawcett-Taylor <ed.fawcetttaylor@helloprima.com>
2025-10-22 11:12:29 +0100

    feat: define specific BigDecimal type

--- lib/avrogen/avro/types/logical.ex
+++ lib/avrogen/avro/types/logical.ex
@@ -41,6 +41,7 @@ defmodule Avrogen.Avro.Types.Logical do
   def module(%{"logicalType" => "decimal", "type" => "bytes"}), do: __MODULE__.Decimal
   def module(%{"logicalType" => "big_decimal", "type" => "string"}), do: __MODULE__.DecimalString
   def module(%{"logicalType" => "big-decimal", "type" => "string"}), do: __MODULE__.DecimalString
+  def module(%{"logicalType" => "big-decimal", "type" => "bytes"}), do: __MODULE__.BigDecimal
 
   def module(%{"logicalType" => "uuid", "type" => "string"}), do: __MODULE__.UUID
 
