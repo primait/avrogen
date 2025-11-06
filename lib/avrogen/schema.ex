2f1adb46d74a551bac72c610affcd049bb7fe25f
Ed Fawcett-Taylor <ed.fawcetttaylor@helloprima.com>
2025-11-06 18:03:36 +0000

    initial commit

--- lib/avrogen/schema.ex
+++ lib/avrogen/schema.ex
@@ -12,19 +12,78 @@ defmodule Avrogen.Schema do
   Get a list of external types referenced in a given record/enum.
   """
   @spec external_dependencies(schema()) :: [String.t()]
-  def external_dependencies(%{"type" => "record", "fields" => fields}) do
-    fields
-    |> Enum.flat_map(fn
-      %{"type" => types} when is_list(types) ->
-        Enum.flat_map(types, fn type -> Avrogen.Types.external_dependencies(type) end)
+  def external_dependencies(%{"namespace" => namespace, "type" => "record", "fields" => fields}) do
+    {external_dependencies, _previously_defined_types} =
+      fields
+      |> Enum.flat_map_reduce(
+        MapSet.new(),
+        fn field, previously_defined_types ->
+          handle_field(field, previously_defined_types, namespace)
+        end
+      )
 
-      %{"type" => type} ->
-        Avrogen.Types.external_dependencies(type)
-    end)
+    external_dependencies |> MapSet.new() |> MapSet.to_list()
+  end
+
+  def external_dependencies(%{"type" => "record", "fields" => fields}) do
+    {external_dependencies, _previously_defined_types} =
+      fields
+      |> Enum.flat_map_reduce(
+        MapSet.new(),
+        fn field, previously_defined_types ->
+          handle_field(field, previously_defined_types, nil)
+        end
+      )
+
+    external_dependencies |> MapSet.new() |> MapSet.to_list()
   end
 
   def external_dependencies(_) do
     []
+  end
+
+  # When the type of the field is a Union, we exclude the previously defined
+  # types and check if those that remain are primitives.
+  @spec handle_field(map(), MapSet.t(String.t()), String.t() | nil) ::
+          {[String.t()], MapSet.t(String.t())}
+  defp handle_field(%{"type" => types}, previously_defined_types, _namespace)
+       when is_list(types) do
+    external_dependencies =
+      types
+      |> MapSet.new()
+      |> MapSet.difference(previously_defined_types)
+      |> Enum.flat_map(&Avrogen.Types.external_dependencies/1)
+
+    {external_dependencies, previously_defined_types}
+  end
+
+  # When the type of the field is a named type, we add it to our set of
+  # previously defined types.
+  defp handle_field(%{"type" => %{"name" => name}}, previously_defined_types, namespace) do
+    fully_qualified_name =
+      if namespace do
+        fqn(%{name: name, namespace: namespace})
+      else
+        fqn(%{name: name})
+      end
+
+    updated_previously_defined_types =
+      MapSet.put(previously_defined_types, fully_qualified_name)
+
+    {[], updated_previously_defined_types}
+  end
+
+  # Otherwise, we check whether this type has been defined previously and, if
+  # not, we check if it's a primitive.
+  defp handle_field(%{"type" => type}, previously_defined_types, _namespace) do
+    external_dependencies =
+      if type in previously_defined_types do
+        []
+      else
+        Avrogen.Types.external_dependencies(type)
+      end
+
+    {external_dependencies, previously_defined_types}
   end
 
   @doc """
