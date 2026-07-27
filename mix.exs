054a93432fe6c9ed50c2aded7affea718e0b4e82
Manuele Dones <manuele.dones@prima.it>
2026-07-27 15:25:55 +0200

    feat: update decimal dependency to allow versions 2.0 and 3.0

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Avrogen.MixProject do
     [
       {:accessible, "~> 0.3"},
       {:credo, "~> 1.7.0", only: [:dev, :test], runtime: false},
-      {:decimal, "~> 3.1"},
+      {:decimal, "~> 2.0 or ~> 3.0"},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:erlavro, "~> 2.9"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
