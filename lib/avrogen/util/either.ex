e55add858fbe1579f67616167e690b316cb6cf9b
Matt Hobbs <matthew.hobbs@prima.it>
2026-08-17 21:49:32 +0100

    undo changes to either

--- lib/avrogen/util/either.ex
+++ lib/avrogen/util/either.ex
@@ -9,7 +9,4 @@ defmodule Avrogen.Util.Either do
   @spec map(either(), fun()) :: either()
   def map({:ok, something}, function), do: {:ok, function.(something)}
   def map({:error, something}, _function), do: {:error, something}
-
-  def unwrap!({:ok, value}), do: value
-  def unwrap!({:error, error}), do: raise(error)
 end
